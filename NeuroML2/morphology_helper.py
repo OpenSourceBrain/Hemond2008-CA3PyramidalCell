b0414eab75a3ce40a716ff679bb9a7a80fda439a
Andris Ecker <ecker.andris@gmail.com>
2016-06-30 20:03:23 +0100

    capools regenerated

--- NeuroML2/morphology_helper.py
+++ NeuroML2/morphology_helper.py
@@ -2,7 +2,7 @@
 """ 
     Helper method to handle Ca++ dynamics in the pyramidal cell model of Hemond 2008 (implemented by Migliore)
     1) the code deletes numberInternalDivisions from annotation (makes it a valid nml file)
-    2) iterates over segments and creates new segment groups based on diameter (the depth of Ca++ shell is changed (from hoc level) according to the diameter in the original code)
+    2) iterates over dendrites and apical_dendrites and calculate the average diam of segments (the depth of Ca++ shell is changed (from hoc level) according to the diameter in the original code)
     Authors: András Ecker, Padraig Gleeson
 """
 
@@ -11,53 +11,49 @@ import neuroml
 import neuroml.loaders as loaders
 import neuroml.writers as writers
 
+import numpy as np
+
 
 def helper_morphology(morph_file):   
 
     doc = loaders.NeuroMLLoader.load(morph_file)
     print("Loaded morphology file from: %s"%morph_file)
     
-    # iterates over segment groups and deletes numberInternalDivisions from annotation (will be valid -> it will be possible to visualize it on OSB)
+    dSegs = {}; dSegs["apical_dendrite"] = []; dSegs["dendrite"] = []
+    # iterates over segment groups
     for segGroup in doc.cells[0].morphology.segment_groups:
+        # deletes numberInternalDivisions from annotation (will be valid -> it will be possible to visualize it on OSB)
         if segGroup.annotation:
             segGroup.annotation = None
-
-            
+       
+        # gets the seg.ids of segmentGroups (dendrite_group and apical_dendrite_group)
+        if segGroup.neuro_lex_id:
+            if segGroup.id[:segGroup.id.rfind('_')] == "apical_dendrite":
+                for member in segGroup.members:
+                    dSegs["apical_dendrite"].append(member.segments)
+            elif segGroup.id[:segGroup.id.rfind('_')] == "dendrite":
+                for member in segGroup.members:
+                    dSegs["dendrite"].append(member.segments)
+    
+    
+    dSegDiams = {}; dSegDiams["apical_dendrite"] = []; dSegDiams["dendrite"] = []
     # iterates over segments and gets the diameter of the segment (distal.diam)
-    dSegDiams = {}
-    dSegGroupDiams = {}
-    capool = ''
     for seg in doc.cells[0].morphology.segments:
         d = seg.distal.diameter
-        if d not in dSegDiams:
-            capool += '\t<HemondCaConcentrationModel id="capool_diam_%s" ion="ca" decayConstant="100ms" restingConc="5e-5mM" shellThickness="%gum"/>\n\n'%(str(d).replace('.', '_'), d/2)
-            dSegDiams[d] = 1
-            dSegGroupDiams[d] = []
-            dSegGroupDiams[d].append(seg.id)
-        else:
-            dSegDiams[d] += 1
-            dSegGroupDiams[d].append(seg.id)
+        if seg.id in dSegs["apical_dendrite"]:
+            dSegDiams["apical_dendrite"].append(d)
+        elif seg.id in dSegs["dendrite"]:
+            dSegDiams["dendrite"].append(d)
             
-       
+
+    capool = ''
+    capool += '\t<HemondCaConcentrationModel id="capool_diam_%s" ion="ca" decayConstant="100ms" restingConc="5e-5mM" shellThickness="%gum"/>\n\n'%(str(np.mean(dSegDiams["dendrite"])).replace('.', '_'), np.mean(dSegDiams["dendrite"])/2)
+    capool += '\t<HemondCaConcentrationModel id="capool_diam_%s" ion="ca" decayConstant="100ms" restingConc="5e-5mM" shellThickness="%gum"/>\n\n'%(str(np.mean(dSegDiams["apical_dendrite"])).replace('.', '_'), np.mean(dSegDiams["apical_dendrite"])/2)
+
     # write out to file... afterwards copy them (manually) to capool.nml TODO: automate this... 
     capools = open("capools.txt", 'w')
     capools.write(capool)
     capools.close()
-    
-    
-    # create segment groups from the segments with same diameter
-    for d, segList in dSegGroupDiams.iteritems():
-        segGroup = neuroml.SegmentGroup(id="diam_%s"%str(d).replace('.', '_'),
-                                        notes="all segments with diam:%g (for implementing Ca++ dynamics)"%d)
-        
-        for id in segList:
-            if id not in [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]:  # these are the seg IDs of segGroup axon_0, which doesn't have Ca++ dynamics (<- yeah I know that it's ugly... András) 
-                member = neuroml.Member(segments=id)
-                segGroup.members.append(member)
-                
-        doc.cells[0].morphology.segment_groups.append(segGroup)
-        print ("segment group added with diam:%g"%d)
-
 
     writers.NeuroMLWriter.write(doc, morph_file)
     print("Saved modified file to: " + morph_file)
